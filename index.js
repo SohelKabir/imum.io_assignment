4556ec2b7b2c5faf8348ec19b88455ccb25bb34f
Sohel Kabir <sohelkabir@Sohels-MacBook-Pro.local>
2023-04-15 03:19:11 +0600

    Add q5 with recursive function

--- index.js
+++ index.js
@@ -5,36 +5,11 @@ import puppeteer from 'puppeteer';
 
 const log = console.log;
 
-const addItems = ($) => {
-  const itemsArr = [];
-
-  // Anyone of these three lines will returns the same result
-  // const $items = $('.ooa-1hab6wx').children('article');
-  // const $$items = $('main[data-testid="search-results"]').children('article');
-  const $items = $('.ooa-1hab6wx > article');
-
-  $items.each(function (i, element) {
-    const id = $(element).attr('data-id');
-    const url = $(element).find('a').attr('href');
-
-    const item = {
-      itemId: id,
-      itemURL: url,
-    };
-    itemsArr.push(item);
-  });
-
-  return itemsArr;
-};
-const getTotalAdsCount = ($) => {
-  return $('main[data-testid="search-results"]').children('article').length;
-};
-
 const scrapeData = async () => {
+  const initURL =
+    'https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-+2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at+%3Adesc';
   try {
-    const res = await axios.get(
-      'https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-+2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at+%3Adesc'
-    );
+    const res = await axios.get(initURL);
 
     const $ = cheerio.load(res.data);
 
@@ -55,11 +30,17 @@ const scrapeData = async () => {
 
     // Question 4
 
-    const struckItem = await scrapeTruckItem($);
+    // const truckItem = await scrapeTruckItem($);
+    // console.log("truckItem:: ", truckItem);
 
-    console.log('========struckItem============================');
-    console.log(struckItem);
-    console.log('===========struckItem=========================');
+    // Question 5
+
+    let allAdsData = [];
+    const allAds = await getALlPagesAds(initURL, allAdsData);
+
+    console.log('===========allAds=========================');
+    console.log(allAds);
+    console.log('============allAds========================');
   } catch (error) {
     log(
       chalk.redBright('Something went wrong! Check below logs for more info!')
@@ -86,6 +67,48 @@ const scrapeData = async () => {
     console.log('error.config:: ', error.config);
   }
 };
+scrapeData();
+
+const getALlPagesAds = async (initURL, allAdsData) => {
+  const res = await axios.get(initURL);
+
+  const $ = cheerio.load(res.data);
+
+  let truckItem = await scrapeTruckItem($);
+  allAdsData.push(truckItem);
+
+  if (getNextPageUrl($)) {
+    const newURL = `https://www.otomoto.pl/${getNextPageUrl($)}`;
+    await getALlPagesAds(newURL, allAdsData);
+  }
+
+  return allAdsData;
+};
+
+const addItems = ($) => {
+  const itemsArr = [];
+
+  // Anyone of these three lines will returns the same result
+  // const $items = $('.ooa-1hab6wx').children('article');
+  // const $$items = $('main[data-testid="search-results"]').children('article');
+  const $items = $('.ooa-1hab6wx > article');
+
+  $items.each(function (i, element) {
+    const id = $(element).attr('data-id');
+    const url = $(element).find('a').attr('href');
+
+    const item = {
+      itemId: id,
+      itemURL: url,
+    };
+    itemsArr.push(item);
+  });
+
+  return itemsArr;
+};
+const getTotalAdsCount = ($) => {
+  return $('main[data-testid="search-results"]').children('article').length;
+};
 
 const getNextPageUrl = ($) => {
   const nextPageUrl = $('.pagination-item__active')
@@ -99,117 +122,75 @@ const getNextPageUrl = ($) => {
     return nextPageUrl;
   } else if ($('.pagination-item__disabled[title="Next Page"]').length) {
     console.log('This is the last page');
-    return `this is the last page`;
+    return undefined;
   } else {
     console.log('No pagination links found');
-    return `No pagination link found`;
+    return undefined;
   }
 };
 
 const scrapeTruckItem = async ($) => {
-  // const itemsArr = [];
-
-  // // Anyone of these three lines will returns the same result
-  // // const $items = $('.ooa-1hab6wx').children('article');
-  // const $items = $('main[data-testid="search-results"]').children('article');
-  // //   const $items = $('ooa-1bmnxg7');
-
-  // $items.each(function (i, element) {
-  //   const id = $(element).attr('data-id');
-  //   const url = $(element).find('a').attr('href');
-  //   const price = $(element).find('.ooa-1bmnxg7').text();
-  //   const adTitle = $(element).find('h2[data-testid="ad-title"]').text();
-
-  //   const item = {
-  //     itemId: id,
-  //     itemURL: url,
-  //     itemPrice: price,
-  //     itemTitle: adTitle,
-  //   };
-  //   itemsArr.push(item);
-  // });
-
-  // return itemsArr;
-
   const itemsArr = [];
 
   const $items = $('main[data-testid="search-results"]').children('article');
 
   const browser = await puppeteer.launch();
   try {
-    // $items.each(async function (i, element) {
-    //   // const id = $(element).attr('data-id');
-    //   const url = $(element).find('a').attr('href');
-    //   // const price = $(element).find('.ooa-1bmnxg7').text();
-    //   // const adTitle = $(element).find('h2[data-testid="ad-title"]').text();
+    const adsURL = [];
+    $items.each(async function (i, element) {
+      // const id = $(element).attr('data-id');
+      const url = $(element).find('a').attr('href').trim();
+      // const price = $(element).find('.ooa-1bmnxg7').text();
+      // const adTitle = $(element).find('h2[data-testid="ad-title"]').text();
 
-    //   if (url) {
-    //     console.log('=========url===========================');
-    //     console.log(url);
-    //     console.log('==========url==========================');
-    //     const page = await browser.newPage();
-    //     await page.goto(url);
-    //     const html = await page.content();
-
-    //     console.log('=======html=============================');
-    //     console.log(html);
-    //     console.log('========html============================');
-    //   }
-    // });
-
-    const page = await browser.newPage();
-    await page.goto(
-      'https://www.otomoto.pl/oferta/mercedes-benz-actros-1845-euro-6-acc-stream-space-retarder-ID6Fratg.html'
-    );
-    const html = await page.content();
-
-    const $htmlData = cheerio.load(html);
-
-    const details = $htmlData('ul.offer-params__list li');
-
-    const data = {};
-
-    const id = $htmlData('#ad_id').first().text().trim();
-
-    const price = $htmlData('.offer-price__number').first().text().trim();
-    const title = $htmlData('.offer-title').first().text().trim();
-
-    data.id = id;
-    data.price = price;
-    data.title = title;
-
-    details.each(function () {
-      const label = $(this).find('span.offer-params__label').text().trim();
-      let value = $(this).find('div.offer-params__value').text().trim();
-      if (value === '') {
-        value = $(this).find('div.offer-params__value a').text().trim();
-      }
-      // data[label] = value
-
-      if (label === 'Rok produkcji') {
-        data['Years of producton'] = value;
-      }
-      if (label === 'Przebieg') {
-        data['mileage'] = value;
-      }
-      if (label === 'Moc') {
-        data['power'] = value;
-      }
+      adsURL.push(url);
     });
 
-    for (let key in data) {
-      console.log('=============data=======================');
-      console.log(key, ' : ', data[key]);
-      console.log('===============data=====================');
+    //TODO make it i <length before final submission, for testing i reduced loading data
+
+    for (let i = 0; i < 3; i++) {
+      const page = await browser.newPage();
+      await page.goto(adsURL[i]);
+      const html = await page.content();
+
+      const $htmlData = cheerio.load(html);
+
+      const id = $htmlData('#ad_id').first().text().trim();
+      const price = $htmlData('.offer-price__number').first().text().trim();
+      const title = $htmlData('.offer-title').first().text().trim();
+      const details = $htmlData('ul.offer-params__list li');
+
+      const data = {};
+
+      data.id = id;
+      data.price = price;
+      data.title = title;
+
+      details.each(function () {
+        const label = $(this).find('span.offer-params__label').text().trim();
+        let value = $(this).find('div.offer-params__value').text().trim();
+        if (value === '') {
+          value = $(this).find('div.offer-params__value a').text().trim();
+        }
+        // data[label] = value
+
+        if (label === 'Rok produkcji') {
+          data['Years of producton'] = value;
+        }
+        if (label === 'Przebieg') {
+          data['mileage'] = value;
+        }
+        if (label === 'Moc') {
+          data['power'] = value;
+        }
+      });
+
+      itemsArr.push(data);
     }
   } catch (error) {
-    console.log('==========errrr==========================');
-    console.log(error);
-    console.log('============errrr========================');
+    console.log('error: ', error);
   }
   await browser.close();
 
   return itemsArr;
 };
-
-scrapeData();
